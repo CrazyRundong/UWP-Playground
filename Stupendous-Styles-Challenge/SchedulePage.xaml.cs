2fafc3ef67a8cda0bca3d16a7d366d0d9940f713
Rundong Lee <lrd.001@163.com>
2016-12-05 22:53:26 +0800

    add slider and some other stuff.

--- Stupendous-Styles-Challenge/SchedulePage.xaml.cs
+++ Stupendous-Styles-Challenge/SchedulePage.xaml.cs
@@ -15,12 +15,12 @@ using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
-namespace Stupendous_Styles_Challenge.Assets {
+namespace Stupendous_Styles_Challenge {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class CoffeePage : Page {
-        public CoffeePage() {
+    public sealed partial class SchedulePage : Page {
+        public SchedulePage() {
             this.InitializeComponent();
         }
     }
